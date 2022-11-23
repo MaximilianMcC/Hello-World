d40c64891011d1fa9ca0530a8029cd1b58840986
Max <max@maximilian.co.nz>
2022-11-24 12:56:29 +1300

    Main class that lets me run hello world

---
+++ src/main/co/nz/maximilian/hello/world/program/Main.java
@@ -0,0 +1,14 @@
+package main.co.nz.maximilian.hello.world.program;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        HelloWorld myFirstHelloWorld = new HelloWorld();
+
+        
+        myFirstHelloWorld.PrintHelloWorld(true);
+
+    }
+    
+}
