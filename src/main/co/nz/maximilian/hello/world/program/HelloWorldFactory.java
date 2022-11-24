607955ee7492652df9d0861ad10109652b18a30b
Max <max@maximilian.co.nz>
2022-11-24 15:27:02 +1300

    Check for what letters and characters are in the hello world string

--- src/main/co/nz/maximilian/hello/world/program/HelloWorldFactory.java
+++ src/main/co/nz/maximilian/hello/world/program/HelloWorldFactory.java
@@ -2,4 +2,42 @@ package main.co.nz.maximilian.hello.world.program;
 
 public class HelloWorldFactory {
     
+    
+    public HelloWorld createHelloWorld() {
+
+        // Find all of the letters and special characters we need to print hello world
+        String helloWorldString = "Hello, world!";
+        for (int i = 0; i < helloWorldString.length(); i++) {
+            
+            // Find uppercase letters
+            for (upperCaseLetters currentUpperCaseLetter : upperCaseLetters.values()) {
+                
+                if (currentUpperCaseLetter.letter == helloWorldString.charAt(i)) {
+    
+                }
+            }
+
+            // Find lowercase letters
+            for (lowerCaseLetters currentLowerCaseLetter : lowerCaseLetters.values()) {
+
+                if (currentLowerCaseLetter.letter == helloWorldString.charAt(i)) {
+
+                }
+            }
+
+            // Find special characters
+            for (specialCharacters currentSpecialCharacter : specialCharacters.values()) {
+
+                if (currentSpecialCharacter.specialCharacter == helloWorldString.charAt(i)) {
+                    
+                }
+            }
+        }
+        
+
+
+
+        return new HelloWorld();
+    }
+
 }
