565601ea47daddee7a3a01f704e5c2889621c665
Max <max@maximilian.co.nz>
2022-11-24 13:00:49 +1300

    I can now identify the source of the HelloWorld class

--- src/main/co/nz/maximilian/hello/world/program/Main.java
+++ src/main/co/nz/maximilian/hello/world/program/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         
-        HelloWorld myFirstHelloWorld = new HelloWorld();
+        // Make a new instance of the "main.co.nz.maximilian.hello.world.program.HelloWorld" class
+        main.co.nz.maximilian.hello.world.program.HelloWorld myFirstHelloWorld = new main.co.nz.maximilian.hello.world.program.HelloWorld();
 
         
         myFirstHelloWorld.PrintHelloWorld(true);
