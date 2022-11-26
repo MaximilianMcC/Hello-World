a658d3489d828b4d3820ceb8766330c36224266b
Max <max@maximilian.co.nz>
2022-11-26 17:49:15 +1300

    Added error catcher to catch any errors

---
+++ src/main/co/nz/maximilian/hello/world/program/ErrorCatcher.java
@@ -0,0 +1,11 @@
+package main.co.nz.maximilian.hello.world.program;
+
+public class ErrorCatcher {
+    
+    public static void CatchError(Exception e) {
+        
+        System.out.println("Bruh hello world error:\n");
+        e.printStackTrace();
+    }
+
+}
