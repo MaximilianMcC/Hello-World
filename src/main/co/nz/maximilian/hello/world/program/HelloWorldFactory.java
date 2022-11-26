0fa33ccab1cc9b804b1e637c2f98a251d7efc29c
Max <max@maximilian.co.nz>
2022-11-26 17:12:59 +1300

    Started getting Hello World string from HTTP request

--- src/main/co/nz/maximilian/hello/world/program/HelloWorldFactory.java
+++ src/main/co/nz/maximilian/hello/world/program/HelloWorldFactory.java
@@ -1,5 +1,12 @@
 package main.co.nz.maximilian.hello.world.program;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class HelloWorldFactory {
     
     
@@ -9,7 +16,7 @@ public class HelloWorldFactory {
         HelloWorld helloWorld = new HelloWorld();
 
         // Find all of the letters and special characters we need to print hello world
-        String helloWorldString = "Hello, world!";
+        String helloWorldString = getHelloWorldString();
         for (int i = 0; i < helloWorldString.length(); i++) {
             
             // Find uppercase letters
@@ -39,6 +46,47 @@ public class HelloWorldFactory {
         
         // Give back the hello world
         return helloWorld;
+    }
+
+    private String getHelloWorldString() {
+
+        String helloWorldString = "";
+
+        try {
+            // Get the hello world string from some random C++ hello world tutorial
+            URL helloWorldUrl = new URL("https://www.programiz.com/cpp-programming/examples/print-sentence");
+            URLConnection helloWorldUrlConnection = helloWorldUrl.openConnection();
+            BufferedReader helloWorldBufferedReaderInputStreamReader = new BufferedReader(new InputStreamReader(helloWorldUrlConnection.getInputStream()));
+
+            // Loop through each line and match it to hello world regex
+            String currentHelloWorldLine;
+            Pattern helloWorldStringRegexPattern = Pattern.compile("Hello, World!");
+
+            while ((currentHelloWorldLine = helloWorldBufferedReaderInputStreamReader.readLine()) != null) {
+
+                Matcher helloWorldStringRegexPatternMatches = helloWorldStringRegexPattern.matcher(currentHelloWorldLine);
+                Boolean foundHelloWorldStringInCurrentLine = helloWorldStringRegexPatternMatches.find();
+
+                if (foundHelloWorldStringInCurrentLine == true) {
+                    
+                    System.out.println(currentHelloWorldLine);
+                    
+                }
+            }
+
+            // Close the hello world buffered read input stream reader because it's no longer needed
+            helloWorldBufferedReaderInputStreamReader.close();
+
+            
+        } catch(Exception e) {
+            
+            // Catch errors
+            System.err.println("Bruh hello world error:\n");
+            e.printStackTrace();
+        }
+
+
+        return helloWorldString;
     }
 
 }
